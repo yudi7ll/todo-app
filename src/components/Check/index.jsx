47ed3a43c9ddb94777c3fca3ad4912635290ac2d
Yudi <madewahyudi22@gmail.com>
2021-01-26 06:23:21 +0800

    minor

--- src/components/Check/index.jsx
+++ src/components/Check/index.jsx
@@ -1,14 +1,14 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
-import CheckIcon from '../../assets/images/icon-check.svg';
+import { CheckIcon } from '../Icons';
 
 import './styles.scss';
 
 function Check({ isCompleted }) {
   return (
     <div className={['check', isCompleted && 'check--active'].join(' ')}>
-      { isCompleted && <img className="check__img" src={CheckIcon} alt="" /> }
+      { isCompleted && <CheckIcon className="check__img" /> }
     </div>
   );
 }
